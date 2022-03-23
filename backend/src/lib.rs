f30845bfdf2f604bfa6e23a5e0b672e5933b238e
ani <97794085+norepimorphism@users.noreply.github.com>
2022-03-23 14:44:26 -0400

    Impl. color and commands in frontend

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,20 @@
-#![feature(iter_advance_by)]
+#![feature(iter_advance_by, let_else)]
 
+pub mod error;
 pub mod interp;
 pub mod lexer;
 pub mod parser;
+
+use error::Error;
+use parser::Ast;
+
+// mod stream;
+
+pub fn process(input: &str) -> Result<Ast, Error> {
+    let tokens = lexer::lex(input)?;
+    // println!("{:?}", tokens);
+    let ast = parser::parse(tokens.into_iter())?;
+    println!("{}", ast);
+
+    Ok(ast)
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,21 +1,124 @@
-use std::io::{self, Write as _};
+#![feature(process_exitcode_placeholder)]
 
-fn main() {
-    println!("Hello, world!");
+use ansi_term::{Color, Style};
+use std::{io::{self, Write as _}, process::ExitCode};
+use supports_color::Stream;
+
+fn main() -> ExitCode {
+    if let Err(_) = main_impl() {
+        eprintln!("fatal error");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
+}
+
+fn main_impl() -> Result<(), ()> {
+    println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
+    let mut interp = oracle_backend::interp::Interp::default();
 
     loop {
-        print!("> ");
+        print!("{} ", color(
+            Stream::Stdout,
+            ">".to_string(),
+            Style::new().bold().fg(Color::Green),
+        ));
 
         let _ = io::stdout().lock().flush();
         let mut input = String::new();
         let _ = io::stdin().read_line(&mut input);
 
-        let tokens = oracle_backend::lexer::lex(input.as_str()).unwrap();
-        println!("{:?}", tokens);
-        let ast = oracle_backend::parser::parse(tokens.into_iter()).unwrap();
-        println!("{:#?}", ast);
-        println!("{}", ast);
-        let mut interp = oracle_backend::interp::Interp::default();
-        interp.eval_ast(ast);
+        let input = input.trim();
+
+        if let Some(cmd) = process_cmd(input) {
+            match cmd {
+                Cmd::GetHelp => {
+                    print_usage();
+                }
+                Cmd::ListAliases => {
+                    println!(
+                        "{}",
+                        interp.aliases
+                            .iter()
+                            .map(|(symbol, operand)| {
+                                format!("{} -> {}", symbol, operand)
+                            })
+                            .collect::<Vec<String>>()
+                            .join("\n")
+                    )
+                }
+                Cmd::Quit => {
+                    return Ok(());
+                }
+            }
+
+            continue;
+        }
+
+        match oracle_backend::process(input) {
+            Ok(ast) => {
+                if let Err(e) = interp.eval_ast(ast) {
+                    print_error(e);
+                }
+            }
+            Err(e) => {
+                print_error(e);
+            }
+        }
     }
+}
+
+fn print_error(e: impl std::error::Error) {
+    eprintln!(
+        "{}: {}",
+        color(
+            Stream::Stderr,
+            "error".to_string(),
+            Style::new().bold().fg(Color::Red),
+        ),
+        e,
+    );
+}
+
+fn color(stream: Stream, input: String, style: Style) -> String {
+    if supports_ansi(stream) {
+        format!("{}", style.paint(input))
+    } else {
+        input
+    }
+}
+
+fn supports_ansi(stream: Stream) -> bool {
+    supports_color::on_cached(stream).map_or(false, |it| it.has_basic)
+}
+
+fn process_cmd(input: &str) -> Option<Cmd> {
+    let (nothing, cmd) = input.split_once(':')?;
+    if !nothing.is_empty() {
+        return None;
+    }
+
+    match cmd {
+        "a" | "list-aliases" => {
+            Some(Cmd::ListAliases)
+        }
+        "h" | "get-help" => {
+            Some(Cmd::GetHelp)
+        }
+        "q" | "quit" => {
+            Some(Cmd::Quit)
+        }
+        _ => None,
+    }
+}
+
+enum Cmd {
+    GetHelp,
+    ListAliases,
+    Quit,
+}
+
+fn print_usage() {
+    todo!()
 }
