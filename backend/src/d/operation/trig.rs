262ba9a4e429cf78f579712355f5db77cf5834d3
ani <97794085+norepimorphism@users.noreply.github.com>
2022-04-03 18:52:33 -0400

    Impl. some trig. functions

---
+++ backend/src/d/operation/trig.rs
@@ -0,0 +1,21 @@
+use crate::{c::Rational, d::{operand::{self, Operand}, operation::Operation}};
+
+macro_rules! unary {
+    ($name:ident, $op:tt) => {
+        pub const $name: Operation = Operation {
+            sig: &[operand::Kind::Rational],
+            exe: |ops| {
+                let a = unsafe { &ops[0].rational };
+
+                Ok(Operand::Rational(Rational { val: a.val.$op() }))
+            },
+        };
+    };
+}
+
+unary!(SIN, sin);
+unary!(COS, cos);
+unary!(TAN, tan);
+unary!(ARCSIN, asin);
+unary!(ARCCOS, acos);
+unary!(ARCTAN, atan);
