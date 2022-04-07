b1af086301cef0355da3242d71b4de3a71ccee1c
ani <97794085+norepimorphism@users.noreply.github.com>
2022-04-07 08:18:14 -0400

    Re-impl. sin, cos, tan

--- backend/src/d/operation.rs
+++ backend/src/d/operation.rs
@@ -1,6 +1,5 @@
-mod arith;
-mod calc;
-mod trig;
+#[macro_use]
+mod macros;
 
 use super::operand::{self, Operand};
 use ahash::RandomState;
@@ -25,12 +24,33 @@ pub static OPERATIONS: HashMap<&'static str, Operation, RandomState> = HashMap::
     // ("sum", arith::SUM),
     // ("prod", arith::PROD),
     // ("int", calc::INT),
+    ("pi", trig::PI),
     ("sin", trig::SIN),
     ("cos", trig::COS),
     ("tan", trig::TAN),
-    ("arcsin", trig::ARCSIN),
-    ("arccos", trig::ARCCOS),
-    ("arctan", trig::ARCTAN),
+    // ("arcsin", trig::ARCSIN),
+    // ("arccos", trig::ARCCOS),
+    // ("arctan", trig::ARCTAN),
     // ("deg", trig::DEG),
     // ("rad", trig::RAD),
 ]);
+
+mod arith {
+    binary_rr_r!(ADD, +);
+    binary_rr_r!(SUB, -);
+    binary_rr_r!(MUL, *);
+    binary_rr_r!(DIV, /);
+}
+
+mod calc {
+
+}
+
+mod trig {
+    use rust_decimal::{Decimal, MathematicalOps as _};
+
+    nullary_r!(PI, Decimal::PI);
+    unary_r_r!(SIN, sin);
+    unary_r_r!(COS, cos);
+    unary_r_r!(TAN, tan);
+}
---
+++ backend/src/d/operation/macros.rs
@@ -0,0 +1,55 @@
+macro_rules! def_operation {
+    ($name:ident, [ $($in_ty:tt),* ], [ $($in_id:tt),* ], $out_op_ty:tt, $out:expr $(,)?) => {
+        pub const $name: $crate::d::operation::Operation = $crate::d::operation::Operation {
+            sig: &[ $($crate::d::operand::Kind::$in_ty),* ],
+            exe: |ops| {
+                $(
+                    // HACK: LOL.
+                    let $in_id = unsafe { &ops[0].rational };
+                    let ops = &ops[1..];
+                )*
+
+                Ok($crate::d::operation::Operand::$out_op_ty($out))
+            },
+        };
+    };
+}
+
+/// Defines a nullary operation that returns a rational.
+macro_rules! nullary_r {
+    ($name:ident, $op:expr) => {
+        def_operation!(
+            $name,
+            [],
+            [],
+            Rational,
+            $crate::c::Rational { val: $op },
+        );
+    };
+}
+
+/// Defines a unary operation that accepts and returns a rational.
+macro_rules! unary_r_r {
+    ($name:ident, $op:tt) => {
+        def_operation!(
+            $name,
+            [Rational],
+            [a],
+            Rational,
+            $crate::c::Rational { val: a.val.$op() },
+        );
+    };
+}
+
+/// Defines a binary operation that acceptes two rationals and returns a rational.
+macro_rules! binary_rr_r {
+    ($name:ident, $op:tt) => {
+        def_operation!(
+            $name,
+            [Rational, Rational],
+            [a, b],
+            Rational,
+            $crate::c::Rational { val: a.val $op b.val },
+        );
+    };
+}
