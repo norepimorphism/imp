d3c6ce11774383ce9ba95bb457c4ab38d194f791
ani <97794085+norepimorphism@users.noreply.github.com>
2022-04-10 17:32:33 -0400

    Improve operation macros

--- backend/src/evaluator/operation.rs
+++ backend/src/evaluator/operation.rs
@@ -2,16 +2,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[macro_use]
-mod macros;
-
 use super::operand::{self, Operand};
 use ahash::RandomState;
 use std::collections::HashMap;
 
 pub struct Operation {
     pub sig: &'static [operand::Kind],
-    pub exe: fn(&[operand::Raw]) -> Result<Operand, Error>,
+    pub exe: fn(&[operand::RawOperand]) -> Result<Operand, Error>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -19,8 +16,8 @@ pub enum Error {}
 
 #[static_init::dynamic]
 pub static OPERATIONS: HashMap<&'static str, Operation, RandomState> = HashMap::from_iter([
-    // ("simplify", todo!()),
-    // ("solve", todo!()),
+    ("e", konst::E),
+    ("pi", konst::PI),
     ("add", arith::ADD),
     ("sub", arith::SUB),
     ("mul", arith::MUL),
@@ -28,7 +25,6 @@ pub static OPERATIONS: HashMap<&'static str, Operation, RandomState> = HashMap::
     // ("sum", arith::SUM),
     // ("prod", arith::PROD),
     // ("int", calc::INT),
-    ("pi", trig::PI),
     ("sin", trig::SIN),
     ("cos", trig::COS),
     ("tan", trig::TAN),
@@ -39,11 +35,77 @@ pub static OPERATIONS: HashMap<&'static str, Operation, RandomState> = HashMap::
     // ("rad", trig::RAD),
 ]);
 
+macro_rules! access_operand {
+    ($parent:expr, Rational) => {
+        $parent.rational
+    };
+    ($parent:expr, StrLit) => {
+        $parent.str_lit
+    };
+    ($parent:expr, Symbol) => {
+        $parent.symbol
+    }
+}
+
+macro_rules! def_operation {
+    ($name:ident, [ $( $in_id:tt : $in_ty:ident ),* ], $out_op_ty:tt, $out:expr $(,)?) => {
+        pub const $name: $crate::evaluator::operation::Operation = $crate::evaluator::operation::Operation {
+            sig: &[ $($crate::evaluator::operand::Kind::$in_ty),* ],
+            exe: |
+                #[allow(unused_variables)]
+                ops
+            | {
+                $(
+                    let $in_id = unsafe { &*access_operand!(&ops[0], $in_ty) };
+                    // HACK: LOL.
+                    #[allow(unused_variables)]
+                    let ops = &ops[1..];
+                )*
+
+                Ok($crate::evaluator::operation::Operand::$out_op_ty(
+                    $out( $($in_id),* )
+                ))
+            },
+        };
+    };
+}
+
+mod konst {
+    use rust_decimal::Decimal;
+
+    macro_rules! def_constant {
+        ($name:ident, $val:expr) => {
+            def_operation!(
+                $name,
+                [],
+                Rational,
+                || crate::parser::Rational { val: $val },
+            );
+        };
+    }
+
+    def_constant!(E, Decimal::E);
+    def_constant!(PI, Decimal::PI);
+}
+
 mod arith {
-    binary_rr_r!(ADD, +);
-    binary_rr_r!(SUB, -);
-    binary_rr_r!(MUL, *);
-    binary_rr_r!(DIV, /);
+    use crate::parser::Rational;
+
+    macro_rules! def_infix_binary {
+        ($name:ident, $op:tt) => {
+            def_operation!(
+                $name,
+                [a: Rational, b: Rational],
+                Rational,
+                |a: &Rational, b: &Rational| Rational { val: a.val $op b.val },
+            );
+        };
+    }
+
+    def_infix_binary!(ADD, +);
+    def_infix_binary!(SUB, -);
+    def_infix_binary!(MUL, *);
+    def_infix_binary!(DIV, /);
 }
 
 mod calc {
@@ -51,10 +113,21 @@ mod calc {
 }
 
 mod trig {
-    use rust_decimal::{Decimal, MathematicalOps as _};
+    use crate::parser::Rational;
+    use rust_decimal::MathematicalOps as _;
 
-    nullary_r!(PI, Decimal::PI);
-    unary_r_r!(SIN, sin);
-    unary_r_r!(COS, cos);
-    unary_r_r!(TAN, tan);
+    macro_rules! def_trig_fn {
+        ($name:ident, $fn:ident) => {
+            def_operation!(
+                $name,
+                [a: Rational],
+                Rational,
+                |a: &Rational| Rational { val: a.val.$fn() },
+            );
+        };
+    }
+
+    def_trig_fn!(SIN, sin);
+    def_trig_fn!(COS, cos);
+    def_trig_fn!(TAN, tan);
 }
