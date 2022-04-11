e12e3385af5abd65cca6efa793eac23a3468c294
ani <97794085+norepimorphism@users.noreply.github.com>
2022-04-11 08:03:40 -0400

    Break module `imp` into more functions

--- frontend/src/shell/imp.rs
+++ frontend/src/shell/imp.rs
@@ -2,49 +2,56 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use imp_backend::span::Span;
 use super::Shell;
 
 pub fn process(this: &Shell, input: &str) {
-    let result = imp_backend::process(
+    let result = process_through_backend(input);
+
+    handle_backend_result(this, result);
+}
+
+fn process_through_backend(input: &str) -> Result<Vec<imp_backend::evaluator::Output>, Span<imp_backend::Error>> {
+    imp_backend::process(
         input,
         imp_backend::Callbacks {
             inspect_lexer_output: Some(|out| {
-                println!("A: {}", out);
+
             }),
             inspect_parser_output: Some(|out| {
-                println!("C: {}", out);
+
             }),
         },
-    );
+    )
+}
 
+fn handle_backend_result(
+    this: &Shell,
+    result: Result<Vec<imp_backend::evaluator::Output>, Span<imp_backend::Error>>,
+) {
     match result {
-        Ok(op) => {
-            for output in op {
-                print_output(this, output);
-            }
+        Ok(outputs) => {
+            handle_backend_success(this, outputs);
         }
         Err(e) => {
-            use crate::err::{self, Stage};
-            use imp_backend::Error;
-
-            print_span(this, &e.range);
-
-            eprintln!(
-                "{}",
-                err::BackendError {
-                    stage: match e.inner {
-                        Error::Lexer(_) => Stage::Lexer,
-                        Error::Parser(_) => Stage::Parser,
-                        Error::Evaluator(_) => Stage::Evaluator,
-                    },
-                    inner: e.inner,
-                },
-            )
+            handle_backend_error(this, e);
         }
     }
 }
 
+fn handle_backend_success(this: &Shell, outputs: Vec<imp_backend::evaluator::Output>) {
+    for output in outputs {
+        print_output(this, output);
+    }
+}
+
 fn print_output(this: &Shell, output: imp_backend::evaluator::Output) {
+    print_output_eq_sign(this);
+    print_output_value(output);
+    println!()
+}
+
+fn print_output_eq_sign(this: &Shell) {
     print!(
         "  {} ",
         crate::color(
@@ -53,35 +60,76 @@ fn print_output(this: &Shell, output: imp_backend::evaluator::Output) {
             ansi_term::Style::new().bold().fg(this.config.output.color)
         )
     );
+}
 
+fn print_output_value(output: imp_backend::evaluator::Output) {
     match output {
         imp_backend::evaluator::Output::Text(text) => {
-            print!("{}", text);
+            print_output_text(text.as_str());
         }
         imp_backend::evaluator::Output::Graphic => {
-            todo!()
+            print_output_graph(());
         }
     }
+}
 
-    println!()
+fn print_output_text(text: &str) {
+    print!("{}", text);
+}
+
+fn print_output_graph(_: ()) {
+    todo!()
+}
+
+fn handle_backend_error(this: &Shell, e: Span<imp_backend::Error>) {
+    use crate::err::{self, BackendStage};
+        use imp_backend::Error;
+
+        print_span(this, &e.range);
+
+        eprintln!(
+            "{}",
+            err::BackendError {
+                stage: match e.inner {
+                    Error::Lexer(_) => BackendStage::Lexer,
+                    Error::Parser(_) => BackendStage::Parser,
+                    Error::Evaluator(_) => BackendStage::Evaluator,
+                },
+                inner: e.inner,
+            },
+        )
 }
 
 fn print_span(this: &Shell, range: &std::ops::Range<usize>) {
-    // Match the shell prompt (` >`).
-    eprint!("{}", this.prompt_padding());
+    print_span_whitespace(this, range);
+    print_span_underline(this, range);
+}
+
+fn print_span_whitespace(this: &Shell, range: &std::ops::Range<usize>) {
+    // Match the shell prompt (`>`).
+    eprint!(" {}", this.prompt_padding());
     // Print leading whitespace.
     eprint!("{0:<1$}", "", range.start);
-    // Print the span.
+}
+
+fn print_span_underline(this: &Shell, range: &std::ops::Range<usize>) {
     eprintln!(
-        "{0:<^1$}",
+        "{}",
         crate::color(
             supports_color::Stream::Stderr,
-            "^".to_string(),
+            create_span_underline(range),
             ansi_term::Style::new().bold().fg(this.config.spans.color),
         ),
-        range
-            .end
-            .checked_sub(range.start)
-            .expect("span range is inverted"),
     );
+}
+
+fn create_span_underline(range: &std::ops::Range<usize>) -> String {
+    std::iter::repeat('^')
+        .take({
+            range
+                .end
+                .checked_sub(range.start)
+                .expect("span range is inverted")
+        })
+        .collect()
 }
